25401dffa23c3b3dfe4fb4ebbfa2a58f21b97932
Wilfried Tonthe <willytonthe1@gmail.com>
2026-02-14 13:49:56 -0500

    Increase SMTP timeouts to fix connection timeout on Render

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -7,7 +7,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
-  }
+  },
+  connectionTimeout: 60000,
+  greetingTimeout: 30000,
+  socketTimeout: 60000
 });
 
 const sendEmail = async (to, subject, html) => {
